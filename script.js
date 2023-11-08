beaae407570ba98f69c4cfc8070909a702df54f1
Sh1v4nk <shivankpandey113@gmail.com>
2023-11-08 21:47:18 +0530

    Implemented task deletion functionality

--- script.js
+++ script.js
@@ -8,8 +8,31 @@ form.addEventListener("submit", function (e) {
   if (taskInput.value.trim() !== "") {
     const newTask = document.createElement("li");
     newTask.className = "task";
-    newTask.innerHTML = `<input class="check-btn" type="checkbox">${taskInput.value}`;
+    newTask.innerHTML = `<input class="check-btn" type="checkbox"><span>${taskInput.value}</span><button class="delete-btn"><span class="material-symbols-outlined">delete</span></button>`;
+
+    const deleteBtn = newTask.querySelector(".delete-btn");
+    deleteBtn.addEventListener("click", function () {
+      newTask.parentNode.removeChild(newTask);
+    });
+
     taskList.appendChild(newTask);
     taskInput.value = "";
   }
 });
+
+document.querySelector(".task-list").addEventListener("change", function (event) {
+    if (
+      event.target.classList.contains("check-btn") &&
+      event.target.type === "checkbox"
+    ) {
+      let listItem = event.target.nextElementSibling;
+      switch (event.target.checked) {
+        case true:
+          listItem.style.textDecoration = "line-through";
+          break;
+        case false:
+          listItem.style.textDecoration = "none";
+          break;
+      }
+    }
+  });
