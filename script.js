5042620ec7a1fbeea2394b337f61fa1dae9b0362
Sh1v4nk <shivankpandey113@gmail.com>
2023-11-11 17:59:12 +0530

    Display 'Completed' status for checked tasks

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ form.addEventListener("submit", function (e) {
   if (taskInput.value.trim() !== "") {
     const newTask = document.createElement("li");
     newTask.className = "task";
-    newTask.innerHTML = `<input class="check-btn" type="checkbox"><span>${taskInput.value}</span><button class="delete-btn"><span class="material-symbols-outlined">delete</span></button>`;
+    newTask.innerHTML = `<input class="check-btn" type="checkbox"><span>${taskInput.value}</span><span class="right-style"><span class="task-done">&#10004 Completed</span><button class="delete-btn"><span class="material-symbols-outlined">delete</span></button></span>`;
 
     const deleteBtn = newTask.querySelector(".delete-btn");
     deleteBtn.addEventListener("click", function () {
@@ -21,18 +21,19 @@ form.addEventListener("submit", function (e) {
 });
 
 document.querySelector(".task-list").addEventListener("change", function (event) {
-    if (
-      event.target.classList.contains("check-btn") &&
-      event.target.type === "checkbox"
-    ) {
-      let listItem = event.target.nextElementSibling;
-      switch (event.target.checked) {
-        case true:
-          listItem.style.textDecoration = "line-through";
-          break;
-        case false:
-          listItem.style.textDecoration = "none";
-          break;
+    const target = event.target;
+
+    if (target.classList.contains("check-btn") && target.type === "checkbox") {
+      let listItem = target.closest(".task");
+      let taskText = listItem.querySelector("span:not(.right-style)");
+      let taskDone = listItem.querySelector(".task-done");
+
+      if (target.checked) {
+        taskText.style.textDecoration = "line-through";
+        taskDone.style.display = "block";
+      } else {
+        taskText.style.textDecoration = "none";
+        taskDone.style.display = "none";
       }
     }
   });
